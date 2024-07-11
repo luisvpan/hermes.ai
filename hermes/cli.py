b8c9c2c8353e44347854e5c3d6a865825496fdc2
geperezz <gph393@gmail.com>
2024-07-11 19:23:33 -0400

    Import only the super hermes builder

--- hermes/cli.py
+++ hermes/cli.py
@@ -3,9 +3,7 @@ import os
 import click
 import tkinter as tk
 
-from .hermes_board import WhiteboardApp
-from .model.hermes_builder import ModelBuilder
-from .model.super_hermes_builder import ModelBuilder as SuperModelBuilder
+from .model.super_hermes_builder import ModelBuilder
 
 @click.group()
 def hermes() -> None:
@@ -13,7 +11,7 @@ def hermes() -> None:
 
 @hermes.command('build')
 def build_model() -> None:
-    model = SuperModelBuilder().build_model()
+    model = ModelBuilder().build_model()
     model.save(os.path.join(os.path.dirname(__file__), './model/super_hermes.keras'))
 
 
