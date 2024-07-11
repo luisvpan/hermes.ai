f2bda4053ab8fead0a97fc86a89228ce1d5e840b
geperezz <gph393@gmail.com>
2024-07-11 19:20:49 -0400

    Update builder

--- hermes/model/super_hermes_builder.py
+++ hermes/model/super_hermes_builder.py
@@ -1,71 +1,113 @@
 import os
+from typing import Any, Generator
 
-import keras
-import pandas
+import tensorflow as tf
+from keras.api.models import Sequential
+from keras.api.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+import numpy as np
 
-class ModelBuilder():
+DATASET_PATH = os.path.join("src", "dataset")
+
+
+class ModelBuilder:
     def __init__(self) -> None:
-        self.model: keras.models.Sequential | None = None
+        self.model: Sequential | None = None
 
-    def build_model(self) -> keras.models.Sequential:
-        self.model = keras.models.Sequential()
+    def build_model(self) -> Sequential:
         self._setup_model_shape()
         self._train_model()
-        self._evaluate_model()
 
         model = self.model
         self.model = None
         return model
-    
+
     def _setup_model_shape(self) -> None:
-        self.model.add(keras.Input(shape=(28, 28, 1)))
-        
-        self.model.add(keras.layers.Conv2D(32, 3, activation='relu'))
-        self.model.add(keras.layers.MaxPooling2D(2, 2))
-        self.model.add(keras.layers.Conv2D(64, 3, activation='relu'))
-        
-        self.model.add(keras.layers.Flatten())
-        
-        self.model.add(keras.layers.Dense(128, activation='relu'))
-        self.model.add(keras.layers.Dropout(0.20))
-        self.model.add(keras.layers.Dense(26, activation='softmax'))
+        self.model = Sequential()
+        self.model.add(
+            Conv2D(
+                32, (3, 3), input_shape=(28, 28, 1), activation="relu", padding="same"
+            )
+        )
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2))
+        self.model.add(Conv2D(64, (3, 3), activation="relu", padding="same"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2))
+        self.model.add(Conv2D(128, (3, 3), activation="relu", padding="same"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2))
+        self.model.add(Flatten())
+        self.model.add(Dense(128, activation="relu"))
+        self.model.add(Dropout(0.2))
+        self.model.add(Dense(64, activation="relu"))
+        self.model.add(Dense(50, activation="softmax"))
+        self.model.compile(
+            loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+        )
+
+    def _calculate_number_of_categories(self) -> int:
+        return len(
+            list(
+                filter(
+                    lambda filename: filename.endswith(".npy"), os.listdir(DATASET_PATH)
+                )
+            )
+        )
+
+    def _find_categories(self) -> Generator[Any, Any, None]:
+        categories_filenames = filter(
+            lambda filename: filename.endswith(".npy"), sorted(os.listdir(DATASET_PATH))
+        )
+        for category_filename in categories_filenames:
+            category_filepath = os.path.join(DATASET_PATH, category_filename)
+            yield category_filename, np.load(category_filepath)
+
+    def _generate_training_sample(self):
+        num_categories = self._calculate_number_of_categories()
+        num_examples_per_category = 6000
+
+        labels = np.zeros(
+            shape=(
+                num_categories * num_examples_per_category,
+                num_categories,
+            )
+        )
+        examples = np.zeros(
+            shape=(num_categories * num_examples_per_category, 28, 28, 1)
+        )
+
+        for i, (current_category_filename, current_category) in enumerate(
+            self._find_categories()
+        ):
+            print(
+                f"Generating training samples for the category in the file {current_category_filename}"
+            )
+
+            current_category = current_category.astype("float32") / 255
+
+            current_examples = current_category.reshape(
+                current_category.shape[0], 28, 28, 1
+            )
+            examples[
+                i * num_examples_per_category : (i + 1) * num_examples_per_category
+            ] = current_examples[
+                np.random.choice(
+                    current_examples.shape[0],
+                    size=num_examples_per_category,
+                    replace=False,
+                )
+            ]
+
+            current_labels = tf.keras.utils.to_categorical(
+                i * np.ones(num_examples_per_category), num_classes=num_categories
+            )
+            labels[
+                i * num_examples_per_category : (i + 1) * num_examples_per_category
+            ] = current_labels
+
+        return examples, labels
 
     def _train_model(self) -> None:
-        training_data = pandas.read_csv(os.path.join(os.path.dirname(__file__), '../datasets/emnist-letters-train.csv'))
-        normalized_training_images = training_data.iloc[:, 1:]
-        normalized_training_images = normalized_training_images.values.reshape(-1, 28, 28)
-        normalized_training_images = normalized_training_images.astype('float32') / 255
-        normalized_training_labels = keras.utils.to_categorical(
-            training_data.iloc[:, 0] - 1,
-            num_classes=26,
-        )
-
-        self.model.compile(
-            loss='categorical_crossentropy',
-            optimizer='adam',
-            metrics=['accuracy'],
-        )
-
-        self.model.fit(
-            x=normalized_training_images,
-            y=normalized_training_labels,
-            epochs=5,
-            batch_size=20,
-            validation_split=0.2,
-        )
-
-    def _evaluate_model(self) -> None:
-        testing_data = pandas.read_csv(os.path.join(os.path.dirname(__file__), '../datasets/emnist-letters-test.csv'))
-        normalized_testing_images = testing_data.iloc[:, 1:]
-        normalized_testing_images = normalized_testing_images.values.reshape(-1, 28, 28)
-        normalized_testing_images = normalized_testing_images.astype('float32') / 255
-        normalized_testing_labels = keras.utils.to_categorical(
-            testing_data.iloc[:, 0] - 1,
-            num_classes=26,
-        )
-        
-        self.model.evaluate(
-            x=normalized_testing_images,
-            y=normalized_testing_labels,
-            batch_size=20,
-        )
+        for i in range(0, 20):
+            examples, labels = self._generate_training_sample()
+            self.model.fit(x=examples, y=labels, epochs=5, batch_size=64)
